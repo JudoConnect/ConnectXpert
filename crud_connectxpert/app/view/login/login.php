1169baef4c4d94bfcc005a42070047c3a0a1e484
hellen <hellen.roque.ifpr@gmail.com>
2023-06-26 16:49:53 -0300

    Front

--- crud_connectxpert/app/view/login/login.php
+++ crud_connectxpert/app/view/login/login.php
@@ -3,38 +3,43 @@
 #Objetivo: interface para logar no sistema
 
 require_once(__DIR__ . "/../include/header.php");
+echo '<link rel="stylesheet" href="' . BASEURL . '/view/login/loginstyle.css">';
 ?>
-
-<div class="container">
-    <div class="row" style="margin-top: 20px;">
-        <div class="col-6">
-            <div class="alert alert-info">
-                <h4>Informe os dados para logar:</h4>
-                <br>
-
-                <!-- Formulário de login -->
-                <form id="frmLogin" action="./LoginController.php?action=logon" method="POST" >
-                    <div class="form-group">
-                        <label for="txtLogin">Login:</label>
-                        <input type="text" class="form-control" name="login" id="txtLogin"
-                            maxlength="15" placeholder="Informe o login"
-                            value="<?php echo isset($dados['login']) ? $dados['login'] : '' ?>" />        
-                    </div>
-
-                    <div class="form-group">
-                        <label for="txtSenha">Senha:</label>
-                        <input type="password" class="form-control" name="senha" id="txtSenha"
-                            maxlength="15" placeholder="Informe a senha"
-                            value="<?php echo isset($dados['senha']) ? $dados['senha'] : '' ?>" />        
-                    </div>
-
-                    <button type="submit" class="btn btn-success">Logar</button>
-                </form>
-            </div>
+<div class="container" style="background-color: #ff7f32;">
+    <div class="row">
+        <div class="col-6">Connect Xpert
         </div>
-
-        <div class="col-6">
-            <?php include_once(__DIR__ . "/../include/msg.php") ?>
+        <div class="col-6" style="height: 100vh;">
+            <div class="row">
+                <div class="col-12">
+                    <div class="Regular shadow p-3 mb-2 bg-white text-dark rounded " style="height: 100vh;">
+                        <h4>LOGIN</h4>
+                        <br>
+                        <!-- Formulário de login -->
+                        <form id="frmLogin" action="./LoginController.php?action=logon" method="POST">
+                            <div class="form-group">
+                                <label for="txtLogin">Usuario</label>
+                                <input type="text" class="form-control" name="login" id="txtLogin" maxlength="15"
+                                    placeholder="Informe o usuario"
+                                    value="<?php echo isset($dados['login']) ? $dados['login'] : '' ?>" />
+                            </div>
+                            <div class="form-group">
+                                <label for="txtSenha">Senha:</label>
+                                <input type="password" class="form-control" name="senha" id="txtSenha" maxlength="15"
+                                    placeholder="Informe a senha"
+                                    value="<?php echo isset($dados['senha']) ? $dados['senha'] : '' ?>" />
+                            </div>
+                            <button type="submit" class="btn">Entrar
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <?php include_once(__DIR__ . "/../include/msg.php") ?>
+                </div>
+            </div>
         </div>
     </div>
 </div>
