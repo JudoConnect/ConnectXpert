4a339249ca7cd43e92299543fe4f7e446c119a12
hellen <hellen.roque.ifpr@gmail.com>
2023-08-11 16:30:26 -0300

    lalalal

--- crud_connectxpert/app/view/include/menu.php
+++ crud_connectxpert/app/view/include/menu.php
@@ -19,22 +19,25 @@ $isAluno = $acessoCont->usuarioPossuiPapel([UsuarioPapel::ALUNO]);
 <div class="col-xs-12" id="nav-container">
     <div id="itensmenu">
         <nav class="navbar navbar-expand-lg " id="menu">
-                <div class="row justify-content-md-left">
-                    <div id="imgmenu">
-                    <img class="img-responsive" style="  width: 40px; height: 40px; " 
-            src="<?= BASEURL . "/view/img/logocx.png" ?>" />
-                        
-                    </div>
-                </div>
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links" aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"> <img src="../public/menu.svg" id="menuicon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbar-links">
-                <div class="navbar-nav">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="row justify-content-md-left">
+  <a class="navbar-brand" href=""><img class="img-responsive" style="  width: 40px; height: 40px; " 
+            src="<?= BASEURL . "/view/img/logocx.png" ?>" /></a>
+            </div>
 
+
+
+
+
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+                <div class="navbar-nav">
                 <a class="nav-item nav-link" href="<?= HOME_PAGE ?>" 
                 style="color: #ff7f32;font-style: normal; font-size: 18px; font-weight: regular;">Home</a>
+
 <!--Se o tipo de usuario for administrador isso aparece-->
                 <?php if($isAdministrador): ?>
                         <a class="nav-link" href="<?= BASEURL . '/controller/ProdutoController.php?action=list' ?>"
@@ -52,6 +55,10 @@ $isAluno = $acessoCont->usuarioPossuiPapel([UsuarioPapel::ALUNO]);
                         <a class="nav-link" href="<?= BASEURL . '/controller/IeController.php?action=list' ?>"
                          style="color: #ff7f32;font-style: normal; font-size: 18px; font-weight: regular;">Ie</a>
                 <?php endif; ?>
+
+                <a class="nav-link" href="<?= LOGOUT_PAGE ?>" 
+                style="color: #ff7f32;font-style: normal; font-size: 18px; font-weight: regular;">Sair</a>
+
                 </div>
             </div>
         </nav>
