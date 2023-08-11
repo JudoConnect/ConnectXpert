cb440cf2ba3ff4725b382b1b759f23bf0a40b2fe
gabriel <gabrielcastro.developer@gmail.com>
2023-08-11 17:57:25 -0300

    login styles

--- crud_connectxpert/app/view/login/login.php
+++ crud_connectxpert/app/view/login/login.php
@@ -5,58 +5,50 @@
 require_once(__DIR__ . "/../include/header.php");
 echo '<link rel="stylesheet" href="' . BASEURL . '/view/login/loginstyle.css">';
 ?>
-<div class="container" style="background-color: #ff7f32;">
-    <div class="row">
-        <div class="col-6"><img class="foto" style=" width: 500px; height: 500px; left: 736px; top: 108px;" 
-            src="<?= BASEURL . "/view/img/alunologin.png" ?>" />
-        </div>
-        <div class="col-6" style="height: 100vh;">
-            <div class="row">
-                <div class="col-12" style="height: 100vh;">
-                    <div class="Regular shadow p-3 mb-2 bg-white text-dark rounded " style="height: 100vh;">
-                        <h4>LOGIN</h4>
-                        <br>
-                        <!-- Formulário de login -->
-                        <form id="frmLogin" action="./LoginController.php?action=logon" method="POST">
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="login" id="txtLogin" maxlength="15"
-                                    placeholder="Login"
-                                    value="<?php echo isset($dados['login']) ? $dados['login'] : '' ?>" />
-                            </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control" name="senha" id="txtSenha" maxlength="15"
-                                    placeholder="Senha"
-                                    value="<?php echo isset($dados['senha']) ? $dados['senha'] : '' ?>" />
-                            </div>
-                            <div class="form-group">
-                                <label>Selecione o tipo de entrada:</label> </br>
-                                <?php foreach($dados["papeis"] as $papel): ?>
-                                    <div class="form-radio">
-                                        <input type="radio" name="papel" id="<?= 'ckb' . $papel ?>" value="<?= $papel ?>"
-                                            
-                                            <?php
-                                                if(isset($dados['papel']) && $papel == $dados['papel'])
-                                                    echo " checked";
-                                            ?>        
-                                        />
-                                        <label for="<?= 'ckb' . $papel ?>"><?= $papel ?></label>
-                                    </div>
-                                <?php endforeach; ?>
-                            </div>
-                            <button type="submit" class="btn">Entrar </button>
-                        </form>
+<div class="pseudo-body">
+    <div class="col-6"><img class="foto" style=" width: 500px; height: 500px; left: 736px; top: 108px;"
+        src="<?= BASEURL . "/view/img/alunologin.png" ?>" />
+    </div>
+
+    <div class="form-holder">
+        <h4>LOGIN</h4>
+        <br>
+        <!-- Formulário de login -->
+        <form id="frmLogin" action="./LoginController.php?action=logon" method="POST">
+
+            <input type="text" class="form-control" name="login" id="txtLogin" maxlength="15"
+                placeholder="Login"
+                value="<?php echo isset($dados['login']) ? $dados['login'] : '' ?>"
+            />
+
+            <input type="password" class="form-control" name="senha" id="txtSenha" maxlength="15"
+                placeholder="Senha"
+                value="<?php echo isset($dados['senha']) ? $dados['senha'] : '' ?>"
+            />
+
+            <div class="form-group">
+                <label>Selecione o tipo de entrada:</label> </br>
+                <?php foreach($dados["papeis"] as $papel): ?>
+                    <div class="form-radio">
+                        <input type="radio" name="papel" id="<?= 'ckb' . $papel ?>" value="<?= $papel ?>"
+
+                            <?php
+                                if(isset($dados['papel']) && $papel == $dados['papel'])
+                                    echo " checked";
+                            ?>
+                        />
+                        <label for="<?= 'ckb' . $papel ?>"><?= $papel ?></label>
                     </div>
-                </div>
+                <?php endforeach; ?>
             </div>
-            <div class="row">
-                <div class="col">
-                    <?php include_once(__DIR__ . "/../include/msg.php") ?>
-                </div>
+
+            <button type="submit" class="btn">Entrar </button>
+        </form>
+
+        <div class="row">
+            <div class="col">
+                <?php include_once(__DIR__ . "/../include/msg.php") ?>
             </div>
         </div>
     </div>
 </div>
-
-<?php  
-require_once(__DIR__ . "/../include/footer.php");
-?>
