0d663baa442c8f86e28d5e2b21ab938aa146b113
hellen <hellen.roque.ifpr@gmail.com>
2023-08-18 11:19:49 -0300

    Lalalal

--- crud_connectxpert/app/view/login/login.php
+++ crud_connectxpert/app/view/login/login.php
@@ -6,49 +6,53 @@ require_once(__DIR__ . "/../include/header.php");
 echo '<link rel="stylesheet" href="' . BASEURL . '/view/login/loginstyle.css">';
 ?>
 <div class="pseudo-body">
-    <div class="col-6"><img class="foto1" style=" width: 500px; height: 500px; left: 736px; top: 108px;"
-        src="<?= BASEURL . "/view/img/alunologin.png" ?>" />
-    </div>
 
-    <div class="form-holder">
-    <img class="foto" style=" width: 200px; height: 50px; " 
-            src="<?= BASEURL . "/view/img/login.png" ?>" />
-        <br>
-        <!-- Formulário de login -->
-        <form id="frmLogin" action="./LoginController.php?action=logon" method="POST">
-            <input type="text" class="form-control" name="login" id="txtLogin" maxlength="15"
-                placeholder="Login"
-                value="<?php echo isset($dados['login']) ? $dados['login'] : '' ?>"
-            />
+    <div class="row">
         
-            <input type="password" class="form-control" name="senha" id="txtSenha" maxlength="15"
-                placeholder="Senha"
-                value="<?php echo isset($dados['senha']) ? $dados['senha'] : '' ?>"
-            />
+        <div class="col-md-6 d-none d-md-block">
+            <img class="destaque-img" src="<?= BASEURL . "/view/img/alunologin.png" ?>" />
+        </div>
 
-            <div class="form-group">
-                <label>Selecione o tipo de entrada:</label> </br>
-                <?php foreach($dados["papeis"] as $papel): ?>
-                    <div class="form-radio">
-                        <input type="radio" name="papel" id="<?= 'ckb' . $papel ?>" value="<?= $papel ?>"
+ 
+        <div class="col-md-6 form-column">
+            <div class="form-holder p-5">
+                <img class="login-img" src="<?= BASEURL . "/view/img/login.png" ?>" />
+                <br>
+                <!-- Formulário de login -->
+                <form id="frmLogin" action="./LoginController.php?action=logon" method="POST">
+                    <input type="text" class="form-control" name="login" id="txtLogin" maxlength="15" placeholder="Login" value="<?php echo isset($dados['login']) ? $dados['login'] : '' ?>" />
 
-                            <?php
-                                if(isset($dados['papel']) && $papel == $dados['papel'])
-                                    echo " checked";
-                            ?>
-                        />
-                        <label for="<?= 'ckb' . $papel ?>"><?= $papel ?></label>
+                    <input type="password" class="form-control" name="senha" id="txtSenha" maxlength="15" placeholder="Senha" value="<?php echo isset($dados['senha']) ? $dados['senha'] : '' ?>" />
+
+                    <div class="form-group">
+                        <label>Selecione o tipo de entrada:</label> </br>
+                        <?php foreach ($dados["papeis"] as $papel) : ?>
+                            <div class="form-radio">
+                                <input type="radio" name="papel" id="<?= 'ckb' . $papel ?>" value="<?= $papel ?>" <?php
+                                                                                                                    if (isset($dados['papel']) && $papel == $dados['papel'])
+                                                                                                                        echo " checked";
+                                                                                                                    ?> />
+                                <label for="<?= 'ckb' . $papel ?>"><?= $papel ?></label>
+                            </div>
+                        <?php endforeach; ?>
                     </div>
-                <?php endforeach; ?>
-            </div>
 
-            <button type="submit" class="btn">Entrar </button>
-        </form>
+                    <button type="submit" class="btn">Entrar </button>
+                </form>
 
-        <div class="row">
-            <div class="col">
-                <?php include_once(__DIR__ . "/../include/msg.php") ?>
+                <div class="row">
+                    <div class="col">
+                        <?php include_once(__DIR__ . "/../include/msg.php") ?>
+                    </div>
+                </div>
             </div>
         </div>
+
+
     </div>
+
+
+
+
+
 </div>
