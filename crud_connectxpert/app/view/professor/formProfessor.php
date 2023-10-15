eb9f995ef5247238348fa0e10c91a23e10c4bc2c
hellen <hellen.roque.ifpr@gmail.com>
2023-10-15 18:30:29 -0300

    lalalalal

--- crud_connectxpert/app/view/professor/formProfessor.php
+++ crud_connectxpert/app/view/professor/formProfessor.php
@@ -4,7 +4,7 @@
 
 require_once(__DIR__ . "/../include/header.php");
 require_once(__DIR__ . "/../include/menu.php");
-echo '<link rel="stylesheet" href="' . BASEURL . '/view/aluno/alunostyle.css">';
+echo '<link rel="stylesheet" href="' . BASEURL . '/view/professor/profstyle.css">';
  
 ?>
 <div class="container">
@@ -13,7 +13,7 @@ echo '<link rel="stylesheet" href="' . BASEURL . '/view/aluno/alunostyle.css">';
     <?php if($dados['id_professor'] == 0) echo "Inserir"; else echo "Alterar"; ?> Professor <a class="btn-inserir" 
    href="<?= BASEURL ?>/controller/AlunoController.php?action=list"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16"  style="color: rgba(255, 127, 50, 1);" >
    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-</svg></a></h3></div></div>
+</svg></a></h3>
 
 <div class="container">
     
