15bed00b36673a94169a8678aaa46f13ca454fa6
MariagEduarda <maria.glasser.ifpr@gmail.com>
2023-11-08 11:46:11 -0300

    att

--- crud_connectxpert/app/view/frequencia/listFrequencia.php
+++ crud_connectxpert/app/view/frequencia/listFrequencia.php
@@ -9,10 +9,7 @@ echo '<link rel="stylesheet" href="' . BASEURL . '/view/turma/turmastyle.css">';
 ?>
 <div class="container">
     <div class="row" style="margin-top: 10px;">
-        <h3 class="col-4">Dados da Turma<a class="btn-inserir" 
-        href="<?= BASEURL ?>/controller/EncontroController.php?action=list"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16"  style="color: rgba(255, 127, 50, 1);" >
-  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-</svg></a></h3>
+        <h3 class="col-4">Dados da Turma</h3>
         </div>
         <div class="row" style="margin-top: 10px;">
         <div class="col-12">
@@ -38,7 +35,10 @@ echo '<link rel="stylesheet" href="' . BASEURL . '/view/turma/turmastyle.css">';
     <!--div class="container"-->
 
     <div class="row">
-        <h3 class="col-4"> Frequência  </h3>
+        <h3 class="col-4"> Frequência <a class="btn-inserir" 
+        href="<?= BASEURL ?>/controller/EncontroController.php?action=list&id=4"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16"  style="color: rgba(255, 127, 50, 1);" >
+  <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+</svg></a> </h3>
     </div>
     <div class="row" style="margin-top: 10px;" >
         <div class="col-12">
